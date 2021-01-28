b474aab15797e0eeaa28b884310c5225d09a255e
benzkittisak <6206021621082@fitm.kmutnb.ac.th>
2021-01-28 08:34:37 +0700

    create

--- cloneObj.js
+++ cloneObj.js
@@ -7,7 +7,7 @@ const obj1 = {
 }
 
 //const obj2 = obj1;
-const obj2 = Object.assign({},obj1);
+//const obj2 = Object.assign({},obj1);
 //const obj2 = {...obj1};
 obj2.Weight=73;
 console.log(obj1);
